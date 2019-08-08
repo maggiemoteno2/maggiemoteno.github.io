c305e2538a62fe56073d31be3f1a9c2a5728c406
TCGPC1 <you@example.com>
2019-08-08 17:43:39 +0200

    added code

---
+++ frontend-projects/company-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Route } from 'react-router-dom'
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <BrowserRouter>
+    <Route path='/'component={App}/>
+</BrowserRouter>,
+document.getElementById('root')
+);
+
+serviceWorker.unregister();
