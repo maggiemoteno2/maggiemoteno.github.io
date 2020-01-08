3e93a3c552510f0da7dda76cdf4332858460082a
maggiemoteno2 <motenomaggie@gmail.com>
2020-01-08 13:54:14 +0200

    deployed tcg broadcast app

---
+++ broadcasttcg/precache-manifest.a7eed7a265983ea62aa3c2d118ff8ae2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0680346477f54e5314b8693905ff99b",
+    "url": "/maggiemoteno.github.io/broadcasttcg/index.html"
+  },
+  {
+    "revision": "ab168368f4d89783a4f1",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/css/2.fb30deaa.chunk.css"
+  },
+  {
+    "revision": "c9bd64eb5f7bab84cfde",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/css/main.89b385b4.chunk.css"
+  },
+  {
+    "revision": "ab168368f4d89783a4f1",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/js/2.6374101e.chunk.js"
+  },
+  {
+    "revision": "c9bd64eb5f7bab84cfde",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/js/main.b4b5b75c.chunk.js"
+  },
+  {
+    "revision": "cc970bb68d8a30712a71",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/js/runtime-main.9e9d5c86.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/maggiemoteno.github.io/broadcasttcg/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
